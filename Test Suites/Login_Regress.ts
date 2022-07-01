0d4c7a5badd1557628f46af0a8771f5bd7571355
muhammadikbal21_ <muhammadikbal21@gmail.com>
2022-07-01 20:47:28 +0700

    melakukan regression testing dengan test suite pada login

---
+++ Test Suites/Login_Regress.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_Regress</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>776aac28-0c90-42ca-8f92-911ffd63b65f</testSuiteGuid>
+   <testCaseLink>
+      <guid>39bdc57f-9c7e-4244-bd2d-afe6019cb9de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC_BlackPassword</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ee13262b-667c-42b3-8b59-14aba3428e12</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC_BlankUsername</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c62bd179-6215-42df-8160-f732686777d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC_InvalidLogin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>93e11216-7fe1-447f-9e26-a575240505b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC_SuccessLogin</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
