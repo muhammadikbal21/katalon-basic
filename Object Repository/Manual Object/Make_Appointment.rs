34581502e063aca0777b27759d9b4735a2a2c3a9
muhammadikbal21_ <muhammadikbal21@gmail.com>
2022-07-09 16:21:12 +0700

    membuat object secara manual dgn mengambil id element di inspect element

---
+++ Object Repository/Manual Object/Make_Appointment.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Make_Appointment</name>
+   <tag></tag>
+   <elementGuidId>834c3dd7-b966-4851-a21f-7c07db3e2553</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'btn-make-appointment']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>btn-make-appointment</value>
+   </webElementProperties>
+</WebElementEntity>
