c58626b4c0bdebb7242dc6f3cba2939679da3770
muhammadikbal21_ <muhammadikbal21@gmail.com>
2022-07-09 14:43:21 +0700

    add screenshot di tc dan regress agar ditampilkan di reportnya

---
+++ Test Suites/Reporting_Screenshot_Regress.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Reporting_Screenshot_Regress</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>95d0c3d4-ad61-4a7a-ad95-f56bf312040b</testSuiteGuid>
+   <testCaseLink>
+      <guid>b7d66f26-9e27-479f-a413-9dd0cc0cb225</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reporting with Screenshot/Booking</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f3438240-701e-4d02-ab14-cecb74d487a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Reporting with Screenshot/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
